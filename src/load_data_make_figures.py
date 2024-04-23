a5b38af0f104eb2480160b55e9931bfebc8a16d1
stromg <stromg@student.chalmers.se>
2024-04-23 21:37:35 +0200

    fill out code in load_data_make_figures to be able to make plots from the example data

--- src/load_data_make_figures.py
+++ src/load_data_make_figures.py
@@ -1,6 +1,6 @@
 ### Initial imports
 import json
-
+import matplotlib.pyplot as plt
 
 
 
@@ -22,13 +22,35 @@ with open(file_name, "r", encoding="utf-8") as file:
 
 ### Perform analysis and plots
 
-# recover the number of runs per probability (assume the same for all)
+# recover the number of runs per probability, error model, and decoder (assume the same for all)
 runs_per_prob = data[0]['n_run']
+error_model = data[0]['error_model']
+decoder = data[0]['decoder']
+
 
 # prepare xy_map for plotting each code performance, with x = physical error rate and y = logical error rate. 
 
 xy_map = {}
+for run in data:
+    xys = xy_map.setdefault(run['code'], [])
+    xys.append((run['physical_error_rate'], run['logical_failure_rate']))
 
+# format plot
+error_probability_min = 0  # Kan också hämtas ur data för plottning, se label "error_probability"
+error_probability_max = 0.5
+
+
+fig = plt.figure(1, figsize=(12, 9))
+plt.title('Rotated planar code simulation\n({} runs per probability, {} error model, {} decoder)'.format(runs_per_prob, error_model, decoder))
+plt.xlabel('Physical error rate')
+plt.ylabel('Logical failure rate')
+plt.xlim(error_probability_min-0.05, error_probability_max+0.05)
+plt.ylim(-0.05, 0.65)
+# add data
+for code, xys in xy_map.items():
+    plt.plot(*zip(*xys), 'x-', label='{} code'.format(code))
+plt.legend(loc='upper left')
+plt.savefig("test.png")
 
 
 
