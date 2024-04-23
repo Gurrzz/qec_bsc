1e8931415ad6642075fcca3132685d466647da50
stromg <stromg@student.chalmers.se>
2024-04-23 14:23:26 +0200

    add file shell for code to load stored data, perform analysis on it, and make plots

---
+++ src/load_data_make_figures.py
@@ -0,0 +1,35 @@
+### Initial imports
+import json
+
+
+
+
+
+
+
+
+
+
+### Load data
+
+file_name = "simulation_2024-04-23 14:01:51.670805_data.json"
+
+
+
+
+
+
+
+
+### Perform analysis and plots
+
+
+
+
+
+
+
+
+
+
+
