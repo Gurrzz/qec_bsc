dc5538d08304ee5e5e76a1edd70b4d840d305353
stromg <stromg@student.chalmers.se>
2024-04-23 14:48:42 +0200

    start filling in functionality in load_data_make_figures

--- src/load_data_make_figures.py
+++ src/load_data_make_figures.py
@@ -12,19 +12,22 @@ import json
 
 ### Load data
 
-file_name = "simulation_2024-04-23 14:01:51.670805_data.json"
+file_name = "example_data.json"
+data = []
 
-
-
-
-
-
+with open(file_name, "r", encoding="utf-8") as file:
+    for line in file:
+        data.append(json.loads(line.removesuffix('\n')))
 
 
 ### Perform analysis and plots
 
+# recover the number of runs per probability (assume the same for all)
+runs_per_prob = data[0]['n_run']
 
+# prepare xy_map for plotting each code performance, with x = physical error rate and y = logical error rate. 
 
+xy_map = {}
 
 
 
