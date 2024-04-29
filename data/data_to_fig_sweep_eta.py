0ee9004569c556d4eddfbeb872a8f9ab7fd31c27
stromg <stromg@student.chalmers.se>
2024-04-29 12:48:11 +0200

    add additional data analysis, comparison and graph generation

--- data/data_to_fig_sweep_eta.py
+++ data/data_to_fig_sweep_eta.py
@@ -1,12 +1,38 @@
 ### Initial imports
 import json
 import matplotlib.pyplot as plt
+import numpy as np
+
+### Analytical solution
+
+def h_g81(x):
+    numerator = (12*x**2 + 1)**2
+    denominator = (8*x**3 + 6*x)**2
+    return numerator / denominator
+
+def t_g81(x):
+    return (h_g81(x) + 0.5) / (h_g81(x) + 1)
 
 
+def h_XZZX(x):
+    numerator = (12*x**2 + 1)
+    denominator = (8*x**3 + 6*x)
+    return numerator / denominator
+
+def t_XZZX(x):
+    return (h_XZZX(x) + 0.5) / (h_XZZX(x) + 1)
+
+eta_values = np.linspace(1, 1000, 5000)
+probability_values_g81 = t_g81(eta_values)
+probability_values_XZZX = t_XZZX(eta_values)
+
+
+
+# Use the above in the plot below
 
 ### Load data
 
-file_name_1 = "eta_run_2.json"
+file_name_1 = "eta_run_3.json"
 data = []
 
 with open(file_name_1, "r", encoding="utf-8") as file:
@@ -35,19 +61,24 @@ eta_max = 1000
 
 
 fig = plt.figure(1, figsize=(8, 6))
-plt.title('Simulering med ytkod G81, d = 3', fontsize=14)
-plt.xlabel('Z-bias', fontsize=12)
+plt.title('Simulering och analytisk lösning med ytkod G81, d = 3, i särskilda punkten', fontsize=14)
+plt.xlabel('Z-bias [$\eta$]', fontsize=12)
 plt.ylabel('Logisk felsannolikhet', fontsize=12)
-plt.xlim(eta_min-5, eta_max+5)
+plt.xlim(eta_min-5, 500+5)
 plt.ylim(0.45, 0.75)
 
 # add data
 for code, xys in xy_map.items():
-    legend_label = f"{code[15:18]}, {code[31:]}"
-    plt.plot(*zip(*xys), 'x-', label=legend_label)
-# plt.legend(loc='upper right', fontsize=11)
+    legend_label = f"{code[15:18]}, {code[31:]}, simulerad"
+    
+    plt.plot(*zip(*xys), label=legend_label)
+
+plt.plot(eta_values, probability_values_g81, label="$G81$ analytisk")
+plt.plot(eta_values, probability_values_XZZX, c="r", label="$XZZX$ analytisk")
+plt.legend(loc='upper right', fontsize=11)
+
 plt.grid()
-plt.savefig("var_eta_d_3_p_specialpoint_svenska_2.png")
+plt.savefig("var_eta_d_3_p_specialpoint_svenska_3_4.png")
 
 
 
